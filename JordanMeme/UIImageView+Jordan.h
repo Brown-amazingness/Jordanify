5b90077260a7f6f564c81423b618c576abdd57a0
Umar Haroon <iforgottonamethis@gmail.com>
2016-02-28 02:40:50 -0700

    Changing imageView to make viewController have less code

---
+++ JordanMeme/UIImageView+Jordan.h
@@ -0,0 +1,14 @@
+//
+//  UIImageView+Jordan.h
+//  JordanMeme
+//
+//  Created by Umar Haroon on 2/24/16.
+//  Copyright © 2016 Umar Haroon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImageView_Jordan : UIImageView
+-(UIImage *)image;
+
+@end
---
+++ JordanMeme/UIImageView+Jordan.m
@@ -0,0 +1,23 @@
+//
+//  UIImageView+Jordan.m
+//  JordanMeme
+//
+//  Created by Umar Haroon on 2/24/16.
+//  Copyright © 2016 Umar Haroon. All rights reserved.
+//
+
+#import "UIImageView+Jordan.h"
+
+@implementation UIImageView_Jordan
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+-(UIImage *)image{
+    return [UIImage imageNamed:@"jordanHead"];
+}
+@end
